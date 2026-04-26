ea822ca294dae07ffcee97dffa84ed186ce426a9
weijuns <3215613640@qq.com>
2026-04-27 02:39:46 +0800

    Update FeatureManager path to ProgramData

--- AcpiProbe.cs
+++ AcpiProbe.cs
@@ -538,7 +538,7 @@ internal static class AcpiProbe
         string exeDir = AppContext.BaseDirectory;
         string[] fmDirCandidates =
         {
-            @"C:\Program Files\MSI Flux\FeatureManager",                                          // Auto-extracted by MSI Flux
+            @"C:\ProgramData\MSI Flux\FeatureManager",                                          // Auto-extracted by MSI Flux
             System.IO.Path.GetFullPath(System.IO.Path.Combine(exeDir, "FeatureManager")),       // Bundled with MSI GPUSwitch
             @"C:\Program Files (x86)\Feature Manager",                                           // System install
         };
